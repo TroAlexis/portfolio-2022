06d7f1d1152fb3b858ddae5b0325ff992b78d5eb
Alex Troshin <aleksei.troshin@tedo.ru>
2022-12-27 20:47:28 +0700

    style: fix formatting in tsx files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -9,9 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        Initial content
-      </main>
+      <main>Initial content</main>
     </div>
-  )
+  );
 }
