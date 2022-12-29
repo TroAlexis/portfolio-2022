586a15accba8acb49a465b760c7659ee15e652b5
Alex Troshin <aleksei.troshin@tedo.ru>
2022-12-29 16:06:13 +0700

    config: add mantine spacing config

--- config/mantine/index.ts
+++ config/mantine/index.ts
@@ -1,9 +1,11 @@
 import { MantineThemeOverride } from "@mantine/core";
 import { roboto } from "@/config/next/fonts";
 import { mantineComponents } from "@/config/mantine/components";
+import { mantineSpacing } from "@/config/mantine/spacing";
 
 export const mantineTheme: MantineThemeOverride = {
   colorScheme: "dark",
   fontFamily: roboto.style.fontFamily,
   components: mantineComponents,
+  spacing: mantineSpacing,
 };
