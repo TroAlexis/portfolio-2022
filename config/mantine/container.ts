c4ddb0f4fb836ee84eed5317643e4973bc26eee1
Alex Troshin <aleksei.troshin@tedo.ru>
2022-12-29 14:39:05 +0700

    feat: add container config, text and background colors

---
+++ config/mantine/container.ts
@@ -0,0 +1,13 @@
+import { MantineThemeComponents } from "@mantine/styles/lib/theme/types/MantineTheme";
+
+const Container: MantineThemeComponents[string] = {
+  defaultProps: {
+    sizes: {
+      lg: 1120,
+      md: 800,
+      xs: 560,
+    },
+  },
+};
+
+export default Container;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
+import Container from "../config/mantine/container";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider
       theme={{
         colorScheme: "dark",
+        components: {
+          Container,
+        },
       }}
     >
       <Component {...pageProps} />
