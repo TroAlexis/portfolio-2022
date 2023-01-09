bcbb874cea7f37836c7d88b6fd8c5e1aca76a5dd
Alex Troshin <aleksei.troshin@tedo.ru>
2023-01-09 16:52:16 +0700

    feat: add container component

---
+++ components/ui/Container/index.tsx
@@ -0,0 +1,22 @@
+import styles from "./index.module.scss";
+import React, { ComponentProps, ElementType, PropsWithChildren } from "react";
+import { PolymorphicComponentProps } from "@/types/types";
+import clsx from "clsx";
+
+type Props<T extends ElementType> = PolymorphicComponentProps<T> &
+  PropsWithChildren<ComponentProps<T>>;
+
+const DEFAULT_TAG: ElementType = "div";
+
+const Container = <T extends ElementType = typeof DEFAULT_TAG>({
+  as,
+  className,
+  ...props
+}: Props<T>) => {
+  const Component = as || DEFAULT_TAG;
+  const classes = clsx(className, styles.container);
+
+  return <Component className={classes} {...props} />;
+};
+
+export default Container;
