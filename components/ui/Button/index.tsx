45994e59accd605d29b35b9ce6a292298174f922
Alex Troshin <aleksei.troshin@tedo.ru>
2023-01-09 17:27:17 +0700

    feat: style button, add outline variant

--- components/ui/Button/index.tsx
+++ components/ui/Button/index.tsx
@@ -4,17 +4,24 @@ import { ComponentPropsWithoutRef, ElementType } from "react";
 import clsx from "clsx";
 
 export type ButtonProps<T extends ElementType> = PolymorphicComponentProps<T> &
-  ComponentPropsWithoutRef<T>;
+  ComponentPropsWithoutRef<T> & {
+    outline?: boolean;
+  };
 
 const DEFAULT_TAG = "button";
 
 export const Button = <T extends ElementType = typeof DEFAULT_TAG>({
   as,
   className,
+  outline,
   ...props
 }: ButtonProps<T>) => {
   const Component = as || DEFAULT_TAG;
-  const classes = clsx(className, styles.button);
+
+  const modifiers = {
+    [styles.isOutline]: outline,
+  };
+  const classes = clsx(className, styles.button, modifiers);
 
   return <Component className={classes} {...props} />;
 };
