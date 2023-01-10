9a60642e300498f3d470c44b80c3b657341ff406
Alex Troshin <aleksei.troshin@tedo.ru>
2023-01-10 16:15:32 +0700

    refactor: extract hero text into component

---
+++ components/Hero/components/Text/index.tsx
@@ -0,0 +1,42 @@
+import styles from "./index.module.scss";
+import React, { ComponentProps, FC } from "react";
+import clsx from "clsx";
+import { Heading } from "components/ui/Heading";
+import { Text } from "components/ui/Text";
+
+interface Props extends ComponentProps<"div"> {}
+
+export const HeroText: FC<Props> = ({ className, ...props }) => {
+  const classes = clsx(className, styles.text);
+  return (
+    <div className={classes}>
+      <Heading as={"p"} size={"3xl"} className={styles.heading}>
+        Hey there! I’m{" "}
+        <Text as={"span"} accent size={undefined}>
+          Alex Troshin
+        </Text>
+      </Heading>
+      <Heading as={"h1"} size={"2xl"} className={styles.subheading}>
+        A passionate software engineer
+      </Heading>
+      <Text as={"p"} className={styles.description}>
+        Creating{" "}
+        <Text as={"span"} accent>
+          reliable
+        </Text>
+        , everyway{" "}
+        <Text as={"span"} accent>
+          responsive
+        </Text>{" "}
+        web apps
+      </Text>
+      <Text as={"p"} className={styles.description}>
+        and bringing ideas to life with{" "}
+        <Text as={"span"} accent>
+          enthusiasm
+        </Text>
+        .
+      </Text>
+    </div>
+  );
+};
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -2,9 +2,8 @@ import styles from "./index.module.scss";
 import React, { ComponentProps, FC } from "react";
 import { Container } from "components/ui/Container";
 import clsx from "clsx";
-import { Heading } from "components/ui/Heading";
-import { Text } from "components/ui/Text";
 import { HeroSocials } from "components/Hero/components/Socials";
+import { HeroText } from "components/Hero/components/Text";
 
 type Props = ComponentProps<"section">;
 
@@ -14,35 +13,7 @@ export const Hero: FC<Props> = ({ className, ...props }) => {
   return (
     <section className={classes} {...props}>
       <Container className={styles.container}>
-        <div className={styles.text}>
-          <Heading as={"p"} size={"3xl"} className={styles.heading}>
-            Hey there! I’m{" "}
-            <Text as={"span"} accent size={undefined}>
-              Alex Troshin
-            </Text>
-          </Heading>
-          <Heading as={"h1"} size={"2xl"} className={styles.subheading}>
-            A passionate software engineer
-          </Heading>
-          <Text as={"p"} className={styles.description}>
-            Creating{" "}
-            <Text as={"span"} accent>
-              reliable
-            </Text>
-            , everyway{" "}
-            <Text as={"span"} accent>
-              responsive
-            </Text>{" "}
-            web apps
-          </Text>
-          <Text as={"p"} className={styles.description}>
-            and bringing ideas to life with{" "}
-            <Text as={"span"} accent>
-              enthusiasm
-            </Text>
-            .
-          </Text>
-        </div>
+        <HeroText className={styles.text} />
         <HeroSocials className={styles.socials} />
       </Container>
     </section>
