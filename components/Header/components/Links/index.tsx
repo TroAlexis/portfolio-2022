e90265bdbae970bf3da25d42abe92e9794a46c61
Alex Troshin <aleksei.troshin@tedo.ru>
2023-01-10 19:36:56 +0700

    feat: add sections app config

--- components/Header/components/Links/index.tsx
+++ components/Header/components/Links/index.tsx
@@ -3,14 +3,28 @@ import { HeaderLink, HeaderLinkProps } from "components/Header/components/Link";
 
 import styles from "./index.module.scss";
 import clsx from "clsx";
+import { APP_SECTIONS } from "@/config/app/sections";
 
 export type HeaderLinksProps = ComponentProps<"ul">;
 
+const getSectionProps = (
+  name: keyof typeof APP_SECTIONS,
+  props: Partial<HeaderLinkProps>
+) => {
+  const section = APP_SECTIONS[name];
+
+  return {
+    ...props,
+    id: section.id,
+    href: `#${section.id}`,
+  };
+};
+
 const links: HeaderLinkProps[] = [
-  { id: "about", href: "#", children: "About me" },
-  { id: "skills", href: "#", children: "Skills" },
-  { id: "projects", href: "#", children: "Projects" },
-  { id: "experience", href: "#", children: "Experience" },
+  getSectionProps("about", { children: "About me" }),
+  getSectionProps("skills", { children: "Skills" }),
+  getSectionProps("projects", { children: "Projects" }),
+  getSectionProps("experience", { children: "Experience" }),
 ];
 
 export const HeaderLinks: FC<HeaderLinksProps> = ({ className, ...props }) => {
---
+++ config/app/sections.ts
@@ -0,0 +1,6 @@
+export const APP_SECTIONS = {
+  about: { id: "about" },
+  skills: { id: "skills" },
+  projects: { id: "projects" },
+  experience: { id: "experience" },
+};
