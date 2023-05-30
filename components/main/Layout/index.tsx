6accbe0a44746575214f182db121f893ecbba42d
Alexey Troshin <alexey.troshin.dev@gmail.com>
2023-05-30 12:57:39 +0300

    fix: remove dynamic header

--- components/main/Layout/index.tsx
+++ components/main/Layout/index.tsx
@@ -1,12 +1,9 @@
 import { Footer } from "components/main/Footer";
 import { Header } from "components/main/Header";
-import { useHeroHeight } from "components/main/Hero/hooks/useHeroHeight";
 import styles from "components/main/Layout/index.module.scss";
 import { FC, PropsWithChildren } from "react";
 
 export const Layout: FC<PropsWithChildren> = ({ children }) => {
-  useHeroHeight();
-
   return (
     <>
       <Header />
