87590a8f5dff74eacf1c227634d9d8e678cb59b3
Alexey Troshin <alexey.troshin.dev@gmail.com>
2023-05-30 13:34:48 +0300

    fix(About): change description

--- components/main/sections/About/index.tsx
+++ components/main/sections/About/index.tsx
@@ -8,9 +8,10 @@ import { APP_SECTIONS } from "@/config/app/sections";
 interface Props extends ComponentProps<"section"> {}
 
 const paragraphs = [
-  "Salut! I’m Alex, an enthusiastic developer and a cheerful person. Used to work as a teacher of English as a second language, and then got burnt out but found the light of hope in the dark thanks to programming.",
-  "3 years down the road, I am now proud to be a software developer with 2 years of production experience, great passion to programming and writing clean, readable code as well as creating and improving products.",
-  "Developed projects being part of agile teams in a range of spheres: real estate (samolet.ru and the company's internal projects for managers, agents and accounting), energy (being part of PwC technology branch), advertising (luckyads.pro), hotel business (v-victoriya.ru), translations (ligva.net - a startup that didn’t make it, unfortunately), volunteering for a charity organisation do-dom.ru laying the foundation in rebuilding their website.",
+  "I'm Alexey Troshin, former ESL teacher turned frontend developer.",
+  "Although I enjoyed helping students learn, I discovered that my true calling was in building apps with great user experiences. I love diving into code and exploring the latest front-end technologies to create modern and responsive web apps.",
+  "Over the years, I've had the opportunity to work on various projects, including data analysis and predictive modeling platforms, real estate, advertising, hotel business, translations. I'm always looking for new and exciting opportunities to learn and grow as a developer.",
+  "Apart from programming, I love cooking, travelling and playing video games. I believe that a good work-life balance is key to staying motivated and productive in my work.",
 ];
 
 const section = APP_SECTIONS.about;
