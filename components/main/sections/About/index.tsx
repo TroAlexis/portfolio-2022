f7cddcbf31b2cad677333dee0a14805c79eaabaf
Alexey Troshin <alexey.troshin.dev@gmail.com>
2023-06-03 15:31:53 +0300

    fix(content): update content

--- components/main/sections/About/index.tsx
+++ components/main/sections/About/index.tsx
@@ -8,10 +8,10 @@ import { APP_SECTIONS } from "@/config/app/sections";
 interface Props extends ComponentProps<"section"> {}
 
 const paragraphs = [
-  "I'm Alexey Troshin, former ESL teacher turned frontend developer.",
-  "Although I enjoyed helping students learn, I discovered that my true calling was in building apps with great user experiences. I love diving into code and exploring the latest front-end technologies to create modern and responsive web apps.",
+  "I'm Alexey Troshin, a senior software engineer.",
+  "I love diving into code and exploring the latest front-end technologies to create modern and responsive web apps.",
   "Over the years, I've had the opportunity to work on various projects, including data analysis and predictive modeling platforms, real estate, advertising, hotel business, translations. I'm always looking for new and exciting opportunities to learn and grow as a developer.",
-  "Apart from programming, I love cooking, travelling and playing video games. I believe that a good work-life balance is key to staying motivated and productive in my work.",
+  "Apart from programming, I love cooking, travelling, playing video games and table tennis. I believe that a good work-life balance is key to staying motivated and productive in my work.",
 ];
 
 const section = APP_SECTIONS.about;
