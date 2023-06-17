bb1d0a717e73cb774c0146c1140ac3766a0b0997
Alexey Troshin <alexey.troshin.dev@gmail.com>
2023-06-17 15:54:09 +0300

    chore(ProjectCard): add link to project in heading and image

--- components/project/ProjectCard/index.tsx
+++ components/project/ProjectCard/index.tsx
@@ -2,6 +2,7 @@ import clsx from "clsx";
 import styles from "components/project/ProjectCard/index.module.scss";
 import { ProjectLinkSet } from "components/project/ProjectLinkSet";
 import { Heading } from "components/ui/Heading";
+import { Link } from "components/ui/Link";
 import { Text } from "components/ui/Text";
 import Image from "next-image-export-optimizer";
 import React, { ComponentProps, FC } from "react";
@@ -23,20 +24,25 @@ export const ProjectCard: FC<Props> = ({
   const classes = clsx(styles.card, className);
 
   const hasLink = !!links;
+  const [firstLink] = links || [];
 
   return (
     <article className={classes} {...props}>
-      <Image
-        src={cover}
-        loading={"lazy"}
-        width={540}
-        height={360}
-        alt={description}
-        className={styles.image}
-      />
-      <Heading as={"h3"} preset={"h3"} className={styles.heading}>
-        {name}
-      </Heading>
+      <Link href={firstLink.href} plain className={styles.imageWrapper}>
+        <Image
+          src={cover}
+          loading={"lazy"}
+          width={540}
+          height={360}
+          alt={description}
+          className={styles.image}
+        />
+      </Link>
+      <Link href={firstLink.href} plain className={styles.headingWrapper}>
+        <Heading as={"h3"} preset={"h3"} className={styles.heading}>
+          {name}
+        </Heading>
+      </Link>
       <Text size={"sm"} as={"p"} className={styles.description}>
         {description}
       </Text>
